cdb658ed99548c88fbbd1d4101990e45b7a950d1
Emil Terziev <emil_trz@icloud.com>
2022-10-11 11:30:18 +0300

    added functionality to all game related actions

--- gamesplay_app/main/forms.py
+++ gamesplay_app/main/forms.py
@@ -25,3 +25,24 @@ class CreateGameForm(forms.ModelForm):
         model = Game
         fields = '__all__'
 
+
+class EditGameForm(forms.ModelForm):
+    class Meta:
+        model = Game
+        fields = '__all__'
+
+
+class DeleteGameForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for _, field in self.fields.items():
+            field.widget.attrs['disabled'] = 'disabled'
+            field.widget.attrs['readonly'] = 'readonly'
+
+    def save(self, commit=True):
+        self.instance.delete()
+        return self.instance
+
+    class Meta:
+        model = Game
+        fields = '__all__'
--- gamesplay_app/main/views/games.py
+++ gamesplay_app/main/views/games.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from gamesplay_app.main.forms import CreateGameForm
+from gamesplay_app.main.forms import CreateGameForm, EditGameForm, DeleteGameForm
+from gamesplay_app.main.models import Game
 
 
 def create_game(request):
@@ -18,13 +19,84 @@ def create_game(request):
     return render(request, 'create-game.html', context)
 
 
-def details_game(request):
-    return render(request, 'details-game.html')
+def details_game(request, pk):
+    game = Game.objects.get(pk=pk)
+    context = {'game': game}
+    return render(request, 'details-game.html', context)
 
 
-def edit_game(request):
-    return render(request, 'edit-game.html')
+def edit_game(request, pk):
+    game = Game.objects.get(pk=pk)
+
+    if request.method == 'GET':
+        # TODO ! ! !
+        form = EditGameForm(request.GET, instance=game)
+        context = {
+            'form': form,
+        }
+        return render(request, 'edit-game.html', context)
+    else:
+        form = EditGameForm(request.POST, instance=game)
+        if form.is_valid():
+            form.save()
+            return redirect('show dashboard')
+        else:
+            context = {
+                'form': form
+            }
+            return render(request, 'edit-game.html', context)
 
 
-def delete_game(request):
-    return render(request, 'delete-game.html')
+def delete_game(request, pk):
+    game = Game.objects.get(pk=pk)
+    if request.method == 'POST':
+        game.delete()
+        return redirect('show dashboard')
+
+    form = DeleteGameForm(instance=game)
+    context = {'form': form}
+    return render(request, 'delete-game.html', context)
+
+    # game = Game.objects.get(pk=pk)
+    #
+    # if request.method == 'POST':
+    #     form = DeleteGameForm(request.POST, instance=game)
+    #
+    #     if form.is_valid():
+    #         form.save()     # / delete()
+    #         return redirect('show dashboard')
+    #
+    # else:
+    #     form = DeleteGameForm(instance=game)
+    #
+    # context = {
+    #     'form': form,
+    #     'game': game
+    # }
+    # return render(request, 'delete-game.html', context)
+
+#     game = models.GameModel.objects.get(id=game_id)
+#     if request.method == 'POST':
+#         game.delete()
+#         return redirect('dashboard-page')
+#
+#     form = forms.DeleteGameForm(instance=game)
+#     context = {'form': form}
+#     return render(request, 'delete-game.html', context)
+
+# def delete_expense(request, pk):
+#     expense = Expense.objects.get(pk=pk)
+#
+#     if request.method == 'POST':
+#         form = DeleteExpenseForm(request.POST, instance=expense)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('show index')
+#     else:
+#         form = DeleteExpenseForm(instance=expense)
+#
+#     context = {
+#         'form': form,
+#         'expense': expense
+#     }
+#     return render(request, 'expense-delete.html', context)
